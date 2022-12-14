3c7456751ec8f7c2df92f418bab653231b1a8fce
안성현 <ahnsh2748@gmail.com>
2022-12-14 13:57:04 +0900

    김영한jpa2 / 회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+    //스프링이 엔티티매니저 만들어 injection 해주는 어노테이션
+    @PersistenceContext
+    private EntityManager em;
+    
+    //emf 직접 주입받고싶을 때 어노테이션
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
